aed57d55966f8938219504b52d4daf0e2b51aaf5
amadeusolofsson <amadeusolofsson@gmail.com>
2026-02-02 07:14:49 +0100

    Fixade tailwind build

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 trap "kill 0" EXIT
-tailwindcss -i ./static/css/input.css -o ./static/css/output.css --watch &
+tailwindcss -i ./static/css/input.css -o ./static/css/output.css --content "./templates/**/*.html"
 python app.py
 wait
